9c8e8b757f450f23044ac4dcb17827c91a47ef4d
KevinLikesCoding <cyf52mc@163.com>
2026-05-01 17:14:04 +0800

    2026/5/1 commit 1

---
+++ CF/CF2209/E.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using i128 = __int128;
+using u128 = unsigned __int128;
+using f128 = __float128;
+#define FOR(i, n, m) for(int i = (n); i <= (m); i ++)
+#define ROF(i, n, m) for(int i = (n); i >= (m); i --)
+#define REP(i, n) for(int i = 0; i < (n); i ++)
+#define SZ(v) int(v.size())
+#define PII pair<int, int>
+#define FI(v) v.first
+#define SE(v) v.second
+#define endl '\n'
+using VI = vector<int>;
+template < class T >
+using VC = vector<T>;
+#define pb push_back
+#define eb emplace_back
+#define lb lower_bound
+#define ub upper_bound
+#define SHOW(x) #x, '=', x
+template < typename A, typename B >
+inline bool chmax(A & x, B y) { return (x < y ? (x = y, true) : false); }
+template < typename A, typename B >
+inline bool chmin(A & x, B y) { return (x > y ? (x = y, true) : false); }
+template < class T >
+void rd_s(T & x) { cin >> x; }
+void read() {}
+template < class H , class ... T >
+void read(H & h, T & ... t) { rd_s(h); read(t ...); }
+template < class T >
+void pt_s(T x) { cout << x; }
+void print() { pt_s(endl); }
+template < class H , class ... T >
+void print(H h, T ... t) { pt_s(h); if(sizeof ... (t)) pt_s(' '); print(t ...); }
+#define INT(...) int __VA_ARGS__; read(__VA_ARGS__)
+#define UINT(...) uint __VA_ARGS__; read(__VA_ARGS__)
+#define LL(...) ll __VA_ARGS__; read(__VA_ARGS__)
+#define ULL(...) ull __VA_ARGS__; read(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__; read(__VA_ARGS__)
+#define CHAR(...) char __VA_ARGS__; read(__VA_ARGS__)
+const int N = 1e6 + 5;
+int n, q, fail[N], a[N], f[N];
+string s;
+void solve() {
+	read(n, q, s);
+	s = ' ' + s;
+	REP(_, q) {
+		INT(l, r);
+		f[l] = a[l] = 1; fail[l] = f[l - 1] = 0;
+		FOR(i, l + 1, r) {
+			int p = fail[i - 1];
+			while(p && s[l + p] != s[i]) p = fail[l + p - 1];
+			if(s[l + p] == s[i]) p ++;
+			fail[i] = p;
+			a[i] = p ? a[l + p - 1] : i - l + 1;
+			f[i] = f[i - a[i]] + 1;
+		}
+		ll ans = 0;
+		FOR(i, l, r) ans += f[i];
+		print(ans);
+	}
+}
+int main() {
+	ios :: sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	INT(T); while(T --)
+	solve();
+	return 0;
+}
